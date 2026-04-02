0119e2d8adb89573bbad28d7dca74f257b8ecb12
AdministratorAashutoshOswal <oswalah4gmail.com>
2026-04-02 15:20:10 +0530

    Testing

---
+++ LearnCiCdPipeLine8/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnCiCdPipeLine8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("CI/CD Demo Working 🚀");
+        }
+
+        [HttpGet("time")]
+        public IActionResult GetTime()
+        {
+            return Ok(DateTime.Now);
+        }
+    }
+}
